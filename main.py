9f0e94732ac0de2c5d2221ef0843d8d3805b863d
OXKernel <opensource@rdoss.com>
2023-04-26 16:29:13 -0400

    starting API implementation

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/create/{function}")
+def read_item(function: str, id: Union[str, None] = None):
+    if function == "user":
+      return {"function": function, "user_id": id}
+    if function == "loan":
+      return {"function": function, "user_id": id}
+    return {"function":"invalid operation", "user_id": "N/A"}
+
--- test.sh
+++ test.sh
@@ -2,4 +2,7 @@
 # To test docs:
 # http://127.0.0.1:8000/docs
 #
-curl "http://127.0.0.1:8000/items/5?q=somequery"
+curl "http://127.0.0.1:8000/create/user?id=someuser"
+printf "\n";
+curl "http://127.0.0.1:8000/create/loan?id=123"
+printf "\n";
