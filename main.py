c66aea838d0b90ef914961ac5e98308566fd1100
OXKernel <opensource@rdoss.com>
2023-04-26 17:02:10 -0400

    initial API design

--- main.py
+++ main.py
@@ -4,17 +4,30 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Python Loan app"}
 
+@app.get("/create/user")
+def read_item(first_name: Union[str, None] = None, last_name: Union[str, None] = None, user_id: Union[str, None] = None):
+    return {"first_name": first_name, "last_name": last_name, "user_id": user_id}
 
-@app.get("/create/{function}")
-def read_item(function: str, id: Union[str, None] = None):
-    if function == "user":
-      return {"function": function, "user_id": id}
-    if function == "loan":
-      return {"function": function, "user_id": id}
-    return {"function":"invalid operation", "user_id": "N/A"}
+@app.get("/create/loan")
+def read_item(principal: Union[str, None] = None, duration_months: Union[str, None] = None, user_id: Union[str, None] = None):
+    return {"principal": principal, "duration": duration_months, "user_id": user_id}
 
+@app.get("/fetch/loan_sched")
+def read_item(user_id: Union[str, None] = None):
+   return {"user_id": user_id}
+
+@app.get("/fetch/loan_summary")
+def read_item(user_id: Union[str, None] = None):
+   return {"user_id": user_id}
+
+@app.get("/fetch/all_loans")
+def read_item(user_id: Union[str, None] = None):
+   return {"user_id": user_id}
+
+@app.get("/share/{loan_id}")
+def read_item(loan_id: str, user_id: Union[str, None] = None):
+    return {"loan_id": loan_id, "user_id": user_id}
--- test.sh
+++ test.sh
@@ -2,7 +2,15 @@
 # To test docs:
 # http://127.0.0.1:8000/docs
 #
-curl "http://127.0.0.1:8000/create/user?id=someuser"
+curl "http://127.0.0.1:8000/create/user?first_name=old&last_name=dog&user_id=123"
 printf "\n";
-curl "http://127.0.0.1:8000/create/loan?id=123"
+curl "http://127.0.0.1:8000/create/loan?principal=100&duration_months=10&user_id=123"
 printf "\n";
+curl "http://127.0.0.1:8000/fetch/loan_sched?user_id=123"
+printf "\n";
+curl "http://127.0.0.1:8000/fetch/loan_summary?user_id=123"
+printf "\n";
+curl "http://127.0.0.1:8000/fetch/all_loans?user_id=123"
+printf "\n";
+curl "http://127.0.0.1:8000/share/abc?user_id=123"
+
